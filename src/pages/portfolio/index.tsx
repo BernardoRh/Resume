1d00f063fe5079d637a672623c39165e1a501ea4
Bernardo R. Horbach <bernardo.horbach@gmail.com>
2023-01-06 14:37:23 -0300

    Starting the Portifolio page

--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -1,5 +1,44 @@
+import { Dots } from "../../components/dots"
+import { ContainerPortifolio } from "./styles"
+import ImageFundamentosReact from "./img/fundamentos.png"
+import ImageDesafio01React from "./img/desafio01.png"
+
 export function Portfolio(){
     return(
-        <div></div>
+        <ContainerPortifolio>
+            <main>
+                <h1>PROJETOS</h1>
+            </main>
+            <a href="#" target="_blank">
+                <div>
+                    <div>
+                        <div>
+                            <Dots color="yellow" size="md"/>
+                            <Dots color="blue" size="md"/>
+                            <Dots color="pink" size="md"/>
+                        </div>
+                        <h3>FUNDAMENTOS REACT</h3>
+                    </div>
+                    <div>
+                        <img src={ImageFundamentosReact}/>
+                    </div>
+                </div>
+            </a>
+            <a href="#" target="_blank">
+                <div>
+                    <div>
+                        <div>
+                            <Dots color="blue" size="md"/>
+                            <Dots color="yellow" size="md"/>
+                            <Dots color="pink" size="md"/>
+                        </div>
+                        <h3>DESAFIO 01 IGNITE</h3>
+                    </div>
+                    <div>
+                        <img src={ImageDesafio01React}/>
+                    </div>
+                </div>
+            </a>
+        </ContainerPortifolio>
     )
 }
--- src/pages/portfolio/styles.ts
+++ src/pages/portfolio/styles.ts
@@ -1,1 +1,59 @@
 import styled from "styled-components";
+
+export const ContainerPortifolio = styled.div`
+    max-width: 54rem;
+    width: 54rem;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+
+    main {
+        text-align: center;
+    }
+
+    a {
+        text-decoration: none;
+        color: ${props => props.theme['blue-dark-gray']};
+
+        div {
+            display: grid;
+            grid-template-columns: 50%;
+            width: 100%;
+            background: ${props => props.theme['tab-blue-1000']};
+            padding: 1rem;
+            border-radius: 8px;
+            gap: 0.75rem;
+            border: 2px solid ${props => props.theme['blue-dark-gray']};
+            outline: 1px solid ${props => props.theme['tab-blue-1000']};
+            
+            div {
+                display: flex;
+                flex-direction: row;
+                border: none;
+                outline: none;
+                background: transparent;
+                padding: 0;
+
+                h3 {
+                    width: 140%;
+                    text-align: left;
+                }
+            }
+
+            &:last-child {
+                display: flex;
+                flex-direction: column;
+                border: 2px solid ${props => props.theme['blue-dark-gray']};
+                outline: 1px solid ${props => props.theme['tab-blue-1000']};
+                grid-column-start: 1;
+                grid-column-end: second;
+                max-height: 22rem;
+                overflow: hidden;
+
+                img {
+                    object-fit: cover;
+                }
+            }
+        }
+    }
+`
