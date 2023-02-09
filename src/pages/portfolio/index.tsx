fb5746353b967ba211e893c2319a4f6ea6186d12
Bernardo R. Horbach <bernardo.horbach@gmail.com>
2023-02-09 17:41:41 -0300

    Changing portfolio tab, changing tittle and adding a project

--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -1,95 +1,121 @@
 import { Dots } from "../../components/dots"
 import { ContainerPortifolio } from "./styles"
-import ImageFundamentosReact from "./img/fundamentos.png"
-import ImageDesafio01React from "./img/desafio01.png"
-import ImageIgniteDS from "./img/igniteDS.png"
-import ImageIgniteTime from "./img/igniteTime.png"
 
 interface PortfolioProps {
     setedLanguage: string,
 }
 
+const projects = [
+    {
+        title: {
+            before: "IGNITE LAB - DESIGN SYSTEM",
+            ptbr: "",
+            enus: "",
+            after: "",
+        },
+        link: "https://github.com/BernardoRh/ignite-ds",
+        dotsColor: {
+            first: "yellow",
+            second: "pink",
+            third: "blue"
+        } as const,
+        image: "https://i.imgur.com/OP2y3O6.png",
+    },
+    {
+        title: {
+            before: "IGNITE - ",
+            ptbr: "FUNDAMENTOS REACT",
+            enus: "REACT FUNDAMENTALS",
+            after: "",
+        },
+        link: "https://bernardorh.github.io/01-fundamentos-reactjs/",
+        dotsColor: {
+            first: "pink",
+            second: "yellow",
+            third: "blue"
+        } as const,
+        image: "https://i.imgur.com/92PyeQt.png",
+    },
+    {
+        title: {
+            before: "IGNITE - ",
+            ptbr: "DESAFIO",
+            enus: "CHALLENGE",
+            after: " 01 (TO DO)",
+        },
+        link: "https://bernardorh.github.io/01-desafio-ignite-react/",
+        dotsColor: {
+            first: "blue",
+            second: "pink",
+            third: "yellow"
+        } as const,
+        image: "https://i.imgur.com/Us6JxFL.png",
+    },
+    {
+        title: {
+            before: "",
+            ptbr: "TEMPORIZADOR",
+            enus: "TIMER",
+            after: ""
+        },
+        link: "https://bernardorh.github.io/02-ignite-timer/",
+        dotsColor: {
+            first: "yellow",
+            second: "blue",
+            third: "pink"
+        } as const,
+        image: "https://i.imgur.com/W0Rdor8.png",
+    },
+    {
+        title: {
+            before: "",
+            ptbr: "FICHA - ORDEM PARANORMAL RPG (EM ANDAMENTO)",
+            enus: "SHEET - ORDEM PARANORMAL RPG (ONGOING)",
+            after: ""
+        },
+        link: "#",
+        dotsColor: {
+            first: "pink",
+            second: "yellow",
+            third: "blue"
+        } as const,
+        image: "https://i.imgur.com/sgM4mrz.png",
+    },
+]
+
 export function Portfolio({setedLanguage}: PortfolioProps){
     return(
         <ContainerPortifolio>
-            <main>
-                <h1>{setedLanguage == "PT-BR"? "PROJETOS": "PROJECTS"}</h1>
-            </main>
-            <a href="https://github.com/BernardoRh/ignite-ds" target="_blank">
-                <div>
-                    <div>
-                        <div>
-                            <Dots color="yellow" size="md"/>
-                            <Dots color="pink" size="md"/>
-                            <Dots color="blue" size="md"/>
-                        </div>
-                        <h3>IGNITE LAB - DESIGN SYSTEM</h3>
-                    </div>
-                    <div>
-                        <img src={ImageIgniteDS}/>
-                    </div>
-                </div>
-            </a>
-            <a href="https://bernardorh.github.io/01-fundamentos-reactjs/" target="_blank">
-                <div>
-                    <div>
-                        <div>
-                            <Dots color="yellow" size="md"/>
-                            <Dots color="blue" size="md"/>
-                            <Dots color="pink" size="md"/>
-                        </div>
-                        <h3>IGNITE - {setedLanguage == "PT-BR" ? "FUNDAMENTOS REACT" : "REACT FUNDAMENTALS"} </h3>
-                    </div>
-                    <div>
-                        <img src={ImageFundamentosReact}/>
-                    </div>
-                </div>
-            </a>
-            <a href="https://bernardorh.github.io/01-desafio-ignite-react/" target="_blank">
-                <div>
-                    <div>
-                        <div>
-                            <Dots color="blue" size="md"/>
-                            <Dots color="yellow" size="md"/>
-                            <Dots color="pink" size="md"/>
-                        </div>
-                        <h3>IGNITE - {setedLanguage == "PT-BR" ? "DESAFIO" : "CHALLENGE"} 01 (TO DO)</h3>
-                    </div>
-                    <div>
-                        <img src={ImageDesafio01React}/>
-                    </div>
-                </div>
-            </a>
-            <a href="https://bernardorh.github.io/02-ignite-timer/" target="_blank">
-                <div>
-                    <div>
-                        <div>
-                            <Dots color="pink" size="md"/>
-                            <Dots color="yellow" size="md"/>
-                            <Dots color="blue" size="md"/>
-                        </div>
-                        <h3>IGNITE - {setedLanguage == "PT-BR" ? "TEMPORIZADOR" : "TIMER"}</h3>
-                    </div>
-                    <div>
-                        <img src={ImageIgniteTime}/>
-                    </div>
-                </div>
-            </a>
-            <a href="#" target="_blank">
-                <div>
-                    <div>
-                        <div>
-                            <Dots color="yellow" size="md"/>
-                            <Dots color="pink" size="md"/>
-                            <Dots color="blue" size="md"/>
-                        </div>
-                        <h3>{setedLanguage == "PT-BR" ? "FICHA - ORDEM PARANORMAL RPG (EM ANDAMENTO)" : "SHEET - ORDEM PARANORMAL RPG (ONGOING)"}</h3>
-                    </div>
-                    <div>
-                        <img src="https://i.imgur.com/sgM4mrz.png"/>
-                    </div>
-                </div>
-            </a>
+            <>
+                <main>
+                    <h1>{setedLanguage == "PT-BR"? "PROJETOS": "PROJECTS"}</h1>
+                </main>
+                {projects.map(project => {
+                    return(
+                        <a href={project.link} target="_blank">
+                            <div>
+                                <div>
+                                    <div>
+                                        <Dots color={project.dotsColor.first} size="md"/>
+                                        <Dots color={project.dotsColor.second} size="md"/>
+                                        <Dots color={project.dotsColor.third} size="md"/>
+                                    </div>
+                                    <h3>
+                                        {project.title.before}
+                                        {setedLanguage == "PT-BR"?
+                                            project.title.ptbr : 
+                                            project.title.enus 
+                                        }
+                                    </h3>
+                                </div>
+                                <div>
+                                    <img src={project.image}/>
+                                </div>
+                            </div>
+                        </a>
+                    )
+                })}
+            </>
         </ContainerPortifolio>
     )
 }
